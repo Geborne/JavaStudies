abed11e9681f439761657bfaed5c0f94bcd57b9a
Geborne <93268144+Geborne@users.noreply.github.com>
2024-02-19 11:48:37 -0300

    Update 6.IfandElses.java

--- 6.IfandElses.java
+++ 6.IfandElses.java
@@ -1,5 +1,11 @@
 public class IfElse{
   public static void main(String[] arg){
-  System.out.print("Hello")
+    int idade = 15;
+
+    if(idade>=18){
+      System.out.print("You're an adult!");
+    }else{
+      System.out.print("You're a minor!");
+    }
   }
 }
