6e70239c5af8e3582b1b9c138ca383fbb471828b
Geborne <93268144+Geborne@users.noreply.github.com>
2024-02-19 11:58:42 -0300

    Update 7.BooleanIfElse.java

--- 7.BooleanIfElse.java
+++ 7.BooleanIfElse.java
@@ -1,5 +1,10 @@
 public class Boolean{
-  public static void main(String[] args){
-    
+  public static void main(String[] arg){
+    int idade = 15;
+    int quantidadePessoas = 2;
+    if(idade>=18 || quantidadePessoas >1){
+      System.out.print("Welcome, get in!");
+    }else
+      System.out.print("You're a lonely minor!");
   }
 }
