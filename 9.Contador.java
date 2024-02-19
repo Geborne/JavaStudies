a3815b60df57ade2d4cd50360501c62bd19ab597
Geborne <93268144+Geborne@users.noreply.github.com>
2024-02-19 13:08:34 -0300

    Create 9.Contador.java

---
+++ 9.Contador.java
@@ -0,0 +1,11 @@
+public class Contador{
+  public static void main(String[] args){
+    int Contador = 0;
+    while (Contador <=1000){
+      int total =0;
+      total = total + Contador;
+      System.out.println(Contador);
+      Contador++;
+    }
+  }
+}
