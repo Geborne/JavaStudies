9fe8b63572b87a2739cf2dec0e7c0bd185a8ba8d
Geborne <93268144+Geborne@users.noreply.github.com>
2024-02-19 09:50:29 -0300

    Update 4.Conversao.java

--- 4.Conversao.java
+++ 4.Conversao.java
@@ -2,7 +2,9 @@ public class Conversao{
   public static void main (String[] args){
     double valor = 3400.76;
     int valorReal = (int) valor;
+    // Colocando o tipo antes da variável faz o Java forçar a conversão.
 
     System.out.print(valorReal);
+    
   }
 }
